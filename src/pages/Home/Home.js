8ea7215715a4626131f52429f0e20b383f54a883
Max Kulicke <mkulicke@gmail.com>
2020-06-27 20:18:42 -0400

    added blurb

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,6 +5,11 @@ import { H1 } from "../../components/Header";
 import "./style.css"
 
 const circleMax = require("../../assets/images/circleMax.png")
+const blurb = `Full-Stack Web Developer specializing in advanced 
+ReactJS Global State applications. Highly effective at creative 
+problem solving, abstract idea development and detail oriented 
+UX & component design. Excellent collaborator and communicator 
+with ferocious work ethic and thorough attention to detail.`
 
 function Home() {
   return (
@@ -21,6 +26,8 @@ function Home() {
               <h2>Max Kulicke</h2>
               <h5>Full Stack Web Developer</h5>
               <h5>Musician, Producer, Engineer</h5>
+              <br />
+              <h6>{blurb}</h6>
             </div>
           </Container>
         </Col>
